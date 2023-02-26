4b152106b66872367d15700858f5c781247f1afa
RareMojo <nickrejcek@gmail.com>
2023-02-26 08:17:21 -0600

    faster image processing and locating
    changed edge detection to detect edges by contour instead of colors and contours
    
    added cropping for compass, can be used to find player facing, unused
    
    added cropping for health bar, can be used to track life level, unused
    
    added user input for refresh rate and player name, prompted before script starts
    
    made file pathing better

--- src/main/paths.py
+++ src/main/paths.py
@@ -1,27 +1,31 @@
 import os
+import cv2
 
-# Get the absolute path of the current working directory
-cwd = os.getcwd()
+path = os.path.dirname(os.path.abspath(__file__))
 
-# Define the paths using the absolute path of the current working directory
+minimap_path = os.path.join(path, '..', 'images', 'screenshot.png')
+minimap_edges_path = os.path.join(path, '..', 'images', 'minimap_edges.png')
+map_edges_path = os.path.join(path, '..', 'images', 'map_edges.png')
+health_bar_path = os.path.join(path, '..', 'images', 'health_bar.png')
+health_bar_edges_path = os.path.join(path, '..', 'images', 'health_bar_edges.png')
+compass_path = os.path.join(path, '..', 'images', 'compass.png')
+
 mapimage_paths = [
-    os.path.join(cwd, 'src', 'images', 'goblincave1.png'),
-    os.path.join(cwd, 'src', 'images', 'crypt1.png'),
-    os.path.join(cwd, 'src', 'images', 'crypt2.png'),
-    os.path.join(cwd, 'src', 'images', 'crypt3.png'),
-    os.path.join(cwd, 'src', 'images', 'crypt4.png'),
-    os.path.join(cwd, 'src', 'images', 'crypt5.png'),
-    os.path.join(cwd, 'src', 'images', 'crypt6.png'),
+    os.path.join(path, '..', 'images', 'goblincave1.png'),
+    os.path.join(path, '..', 'images', 'crypt1.png'),
+    os.path.join(path, '..', 'images', 'crypt2.png'),
+    os.path.join(path, '..', 'images', 'crypt3.png'),
+    os.path.join(path, '..', 'images', 'crypt4.png'),
+    os.path.join(path, '..', 'images', 'crypt5.png'),
+    os.path.join(path, '..', 'images', 'crypt6.png'),
 ]
+
 map_edges_paths = [
-    os.path.join(cwd, 'src', 'images', 'edge_goblincave1.png'),
-    os.path.join(cwd, 'src', 'images', 'edge_crypt1.png'),
-    os.path.join(cwd, 'src', 'images', 'edge_crypt2.png'),
-    os.path.join(cwd, 'src', 'images', 'edge_crypt3.png'),
-    os.path.join(cwd, 'src', 'images', 'edge_crypt4.png'),
-    os.path.join(cwd, 'src', 'images', 'edge_crypt5.png'),
-    os.path.join(cwd, 'src', 'images', 'edge_crypt6.png'),
+    os.path.join(path, '..', 'images', 'edge_goblincave1.png'),
+    os.path.join(path, '..', 'images', 'edge_crypt1.png'),
+    os.path.join(path, '..', 'images', 'edge_crypt2.png'),
+    os.path.join(path, '..', 'images', 'edge_crypt3.png'),
+    os.path.join(path, '..', 'images', 'edge_crypt4.png'),
+    os.path.join(path, '..', 'images', 'edge_crypt5.png'),
+    os.path.join(path, '..', 'images', 'edge_crypt6.png'),
 ]
-minimap_path = os.path.join(cwd, 'src', 'images', 'screenshot.png')
-minimap_edges_path = os.path.join(cwd, 'src', 'images', 'minimap_edges.png')
-map_edges_path = os.path.join(cwd, 'src', 'images', 'map_edges.png')
